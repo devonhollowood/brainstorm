a07a42b49df9a65faf0524046f4fcce196bd01cf
Devon Hollowood <devonhollowood@gmail.com>
2015-10-27 01:33:08 -0700

    Temporary main fn to help debugging

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,40 @@
 extern crate brainstorm;
 
 fn main() {
-    println!("Hello, world!");
+    let prog = "\
+    /* Hi Bassturds! */\
+    +++++ +++++      loop 10x\
+    [ >              enter loop\
+    +++++ ++       > D = 68\
+    +++++ +++++    > e = 101\
+    +++++ +++++ ++ > v = 118\
+    +++++ +++++ +  > o = 111\
+    +++++ +++++ +  > n = 110\
+    +++            > _ = 32\
+    +++++ +++++ +  > r = 114\
+    +++++ +++++ ++ > u = 117\
+    +++++ +++++ +  > l = 108\
+    +++++ +++++    > e = 101\
+    +++++ +++++ +  > s = 115\
+    +++              ! = 33\
+    <<<<< <<<<< << - decrement counter\
+    ]\
+    >\
+    --.    > D\
+    +.     > e\
+    --.    > v\
+    +.     > o\
+    .      > n\
+    ++.    > _\
+    ++++.  > r\
+    ---.   > u\
+    --.    > l\
+    +.     > e\
+    +++++. > s\
+    +++.     !".to_owned();
+    let lexed = brainstorm::lexer::lex(&prog);
+    let parsed = brainstorm::parser::parse(lexed).unwrap();
+    let mut output = String::new();
+    brainstorm::codegen::generate_asm(&mut output, &parsed);
+    println!("{}", output);
 }
